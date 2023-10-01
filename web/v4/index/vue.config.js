5efbf99276b5ca4a33351411c7aee203ff678add
kuyoru <kuyoru730@gmail.com>
2023-10-01 17:18:43 +0800

    v4构建行为变化

--- web/v4/index/vue.config.js
+++ web/v4/index/vue.config.js
@@ -1,6 +1,5 @@
 const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-    outputDir: 'E:\\Projects\\kacg\\java\\spring_ref\\src\\main\\resources',
     transpileDependencies: [
         'vuetify'
     ],
