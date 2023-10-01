9bed20307f1c1568baa17f2185e6cb94bec6bdf5
kuyoru <kuyoru730@gmail.com>
2023-10-01 16:51:42 +0800

    remove unused files

--- java/v4/src/main/java/com/kuyoru/mapper/UcatMapper.java
+++
@@ -1,9 +0,0 @@
-package com.kuyoru.mapper;
-
-/**
- * @author Kty
- *
- * 用户登记侧Mapper
- */
-public interface UcatMapper {
-}
--- java/v4/src/main/java/com/kuyoru/pojo/Ucat.java
+++
@@ -1,9 +0,0 @@
-package com.kuyoru.pojo;
-
-/**
- * @author Kty
- *
- * 用户登记实体类
- */
-public class Ucat {
-}
