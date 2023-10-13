6e3948d603ad4663a5f59897d52ae1ef509fd116
kamikisho <kuyoru730@gmail.com>
2023-10-13 16:33:00 +0800

    ripple nav list

---
+++ webapps/src/views/developPage/components/ContentFrame.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="d-flex flex-col content-frame">
+    <div class="text-h5 d-flex title-field">
+      <slot name="head"></slot>
+      <span class="mx-2 anchor">#</span>
+    </div>
+    <div class="content-field py-4">
+      <slot name="default"></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss">
+.content-frame {
+  a#title {
+    color: unset;
+  }
+
+  p {
+    margin-bottom: 0.89rem;
+  }
+
+  .title-field {
+    &:hover {
+      .anchor {
+        opacity: 1;
+      }
+    }
+  }
+
+  .content-field {
+    word-break: break-word;
+  }
+
+  .anchor {
+    opacity: 0;
+    transition: opacity .2s ease;
+  }
+}
+</style>
--- webapps/src/views/developPage/subpages/Introduction.vue
+++ webapps/src/views/developPage/subpages/Introduction.vue
@@ -1,13 +1,37 @@
 <template>
-<div>sss</div>
+  <content-frame class="introduction">
+    <template #head>
+      <a id="title" href="/kamikisho/docs/components/introduction#introduction">介绍</a>
+    </template>
+    <template #default>
+      <p>
+        <a class="text-decoration-none"
+           href="https://github.com/kuyoru-kamikisho/kamikisho/tree/main/webapps/src/components">K-UI</a>
+        是专门为 kamikisho系列网站开发的一套基于Vue3的UI组件库，任何 kamikisho 系列网站都应该使用 K-UI 里面的组件进行开发。
+      </p>
+      <p>当然，这些组件都是完全开源的，
+        如果您感觉这些组件做的不错，
+        非常欢迎您在您自己的网站中使用，
+        同样地如果您对这些组件有什么改进建议，
+        也非常欢迎您 <a href="https://github.com/kuyoru-kamikisho/kamikisho/issues">点击这里</a>
+        提出您的宝贵意见。
+      </p>
+      <p>关于这些组件的性能，
+        为了尽可能地提高组件的可定制性，
+        这些组件都采用TSX进行开发，
+        同时也尽量地避免了重复代码和无用代码来减轻每一个组件的代码体量，
+        所有的 kamikisho 系列网站均采用这些组件进行开发，
+        您可以尽情测试。
+      </p>
+    </template>
+  </content-frame>
 </template>
 
-<script>
-export default {
-  name: "Introduction"
-}
+<script setup lang="ts">
+import ContentFrame from "@/views/developPage/components/ContentFrame.vue";
+
 </script>
 
-<style scoped>
+<style lang="scss">
 
 </style>
